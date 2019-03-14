722c28a22d34fe60e5f558bdc18edaae362f4501
it102018 <damian.dudek@interia.eu>
2019-03-14 19:48:25 +0100

    Add births

---
+++ jprogramowania_w_analizie_danych/births.py
@@ -0,0 +1,24 @@
+import re as re
+import numpy as np
+import pandas as pd
+from scipy import stats
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+
+def main():
+    data = pd.read_csv('births.csv', sep = ',')
+
+    hypothesis_value = 10000.0
+
+    #normalized_hypothesis_value = preprocessing.normalize([[10000]])[0][0]
+    #normalized_data = preprocessing.normalize([np.array(data['births'])])[0]
+
+    print(stats.ttest_1samp(a = data['births'], popmean = hypothesis_value))
+    
+    plt.hist(data['births'], rwidth = 0.85)
+    plt.xlabel('Number of births')
+    plt.ylabel('Frequency')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
