5ecac7db3f6c24bbcb0419298ce5a078def9cec2
it102018 <damian.dudek@interia.eu>
2019-03-15 19:54:12 +0100

    Last changes

--- jprogramowania_w_analizie_danych/births.py
+++ jprogramowania_w_analizie_danych/births.py
@@ -12,14 +12,27 @@ def main():
 
     #normalized_hypothesis_value = preprocessing.normalize([[10000]])[0][0]
     #normalized_data = preprocessing.normalize([np.array(data['births'])])[0]
+    #ttest = stats.ttest_1samp(a = normalized_data, popmean = normalized_hypothesis_value)
 
     ttest = stats.ttest_1samp(a = data['births'], popmean = hypothesis_value)
 
-    print(stats.t.cdf(ttest.statistic, ttest.pvalue))
+    #p = stats.t.cdf(ttest.statistic, ttest.pvalue)
+
+    print('statistic: ' + str(ttest.statistic))
+    print('pvalue: ' + str(ttest.pvalue))
+    #print('p: ' + str(p))
+
+    alpha = 0.05
+
+    if ttest.pvalue > alpha:
+        print('Zaakceptuj hipotezę')
+    else:
+        print('Odrzuć hipotezę')
 
     plt.hist(data['births'], rwidth = 0.85)
     plt.xlabel('Number of births')
     plt.ylabel('Frequency')
+    plt.annotate('Badana hipoteza', xy = (10000, 1200), xytext = (11500, 1000), arrowprops = dict(facecolor = 'black', shrink = 0.05))
     plt.show()
 
 if __name__ == "__main__":
