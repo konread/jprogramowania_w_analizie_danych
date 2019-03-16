b187a069d4a1b9f6c013e2d378cc5d7994b44948
it102018 <damian.dudek@interia.eu>
2019-03-16 13:09:06 +0100

    Add gaussian_kde

--- jprogramowania_w_analizie_danych/births.py
+++ jprogramowania_w_analizie_danych/births.py
@@ -8,19 +8,37 @@ from sklearn import preprocessing
 def main():
     data = pd.read_csv('births.csv', sep = ',')
 
+    data['births'] = data['births'].astype('float64')
+
     hypothesis_value = 10000.0
 
     #normalized_hypothesis_value = preprocessing.normalize([[10000]])[0][0]
     #normalized_data = preprocessing.normalize([np.array(data['births'])])[0]
     #ttest = stats.ttest_1samp(a = normalized_data, popmean = normalized_hypothesis_value)
 
+    plt.hist(data['births'], rwidth = 0.85, bins = 25, density = True)
+
+    xmin, xmax = plt.xlim()
+
+    x = np.linspace(xmin, xmax, len(data['births']))
+
+    kde = stats.gaussian_kde(data['births'])
+
+    plt.plot(x, kde(x), color = 'red')
+    plt.xlabel('Number of births')
+    plt.ylabel('Frequency')
+    plt.annotate('Badana hipoteza', xy = (10000, 0.0001), xytext = (12000, 0.0001), arrowprops = dict(facecolor = 'black', shrink = 0.05))
+    plt.show()
+
     ttest = stats.ttest_1samp(a = data['births'], popmean = hypothesis_value)
 
-    #p = stats.t.cdf(ttest.statistic, ttest.pvalue)
+    df = len(data['births']) - 1
+
+    p = stats.t.cdf(x = ttest.statistic, df = df) * 2
 
     print('statistic: ' + str(ttest.statistic))
     print('pvalue: ' + str(ttest.pvalue))
-    #print('p: ' + str(p))
+    print('p: ' + str(p))
 
     alpha = 0.05
 
@@ -28,12 +46,6 @@ def main():
         print('Zaakceptuj hipotezę')
     else:
         print('Odrzuć hipotezę')
-
-    plt.hist(data['births'], rwidth = 0.85)
-    plt.xlabel('Number of births')
-    plt.ylabel('Frequency')
-    plt.annotate('Badana hipoteza', xy = (10000, 1200), xytext = (11500, 1000), arrowprops = dict(facecolor = 'black', shrink = 0.05))
-    plt.show()
 
 if __name__ == "__main__":
     main()
