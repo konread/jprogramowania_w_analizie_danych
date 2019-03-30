1a1278b4e5f476b62d863c498b661def7eeaa51a
konread <slaby.konrad@gmail.com>
2019-03-30 01:10:49 +0100

    init file

--- Zadanie2/Zadanie2/Zadanie2.py
+++ Zadanie2/Zadanie2/Zadanie2.py
@@ -1,1 +1,19 @@
-print("OK")
+import pandas as pd
+
+dataset = pd.read_csv('AirQualityUCI.csv', ';')
+dataset.drop("Date", axis=1, inplace=True)
+dataset.drop("Time", axis=1, inplace=True)
+dataset.drop("Unnamed: 15", axis=1, inplace=True)
+dataset.drop("Unnamed: 16", axis=1, inplace=True)
+
+percentAnalysis = [["ColumnName","Percent %"]]
+for colName in dataset:
+    missingValueCounter = 0
+    for value in dataset[colName]:
+        if value == -200:
+            missingValueCounter += 1
+    total=len(dataset[colName])
+    percentAnalysis.append([colName,str(round((missingValueCounter/total)*100,2))])
+
+for row in percentAnalysis:
+    print("{: >15} {: >15}".format(*row))
